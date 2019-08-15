ec88a0f8d2aaf52059a385b4ec83c4daccfc6e70
elihe999 <eli.he999@hotmail.com>
2019-08-15 14:22:00 +0800

    <chore>Update example

--- example.js
+++ example.js
@@ -1,15 +1,25 @@
 
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 
 (async () => {
   const browser = await puppeteer.launch({
-    executablePath: "/snap/bin/chromium",
+    executablePath: "/opt/google/chrome/chrome",
+    // executablePath: "/usr/bin/chromium-browser",
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
     headless: false
   });
   const page = await browser.newPage();
-  await page.goto('https://192.168.92.48');
-  // await page.screenshot({path: 'example.png'});
-
+  await page.goto('http://192.168.92.48');
+  await page.waitFor(5000)
+  await page.type(
+          "#control-pad > div:nth-child(1) > div.content > input", "admin"
+  )
+  await page.type(
+          "#control-pad > div:nth-child(2) > div.content > input", "123456"
+  )
+  await page.click('button[class="gwt-Button"]')
+  //.gwt-ListBox // language
+  await page.waitFor(2000)
+  await page.screenshot({path: 'example.png'});
   await browser.close();
 })();
